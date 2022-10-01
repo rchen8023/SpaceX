b28503ffe3d3e22be768747af51595062acef717
Rui Chen <sonya123@foxmail.com>
2022-10-01 16:31:29 -0600

    Create Main content, SatSetting

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Header from './Header';
 import Footer from './Footer';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
