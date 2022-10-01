b2b1331b4328f33f76af69ed7ed96b19e3c33b26
Rui Chen <sonya123@foxmail.com>
2022-10-01 15:49:33 -0600

    Create Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2022 StarLink Tracker. All Rights Reserved. Website Made by Rui Chen
+            </footer>
+        );
+    }
+}
+
+export default Footer;
